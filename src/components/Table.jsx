c64b16342780f465785fe93d6bd5f777615f7221
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-04-14 22:41:54 -0300

    adding basic files

---
+++ src/components/Table.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/Table.css';
+
+class Table extends React.Component{
+    render(){
+        return(
+            <h1>That is the table</h1>
+        )
+    }
+}
+
+export default Table;
